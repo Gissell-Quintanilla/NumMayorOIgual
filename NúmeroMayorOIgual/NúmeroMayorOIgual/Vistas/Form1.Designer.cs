868df822a0483408c4a528f1396321180c1663c4
Karla Gissell Benítez Quintanilla <124007184+Gissell-Quintanilla@users.noreply.github.com>
2023-01-31 08:34:57 -0600

    Commit1

---
+++ NúmeroMayorOIgual/NúmeroMayorOIgual/Vistas/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace NúmeroMayorOIgual
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumero1 = new System.Windows.Forms.Label();
+            this.lblNumero2 = new System.Windows.Forms.Label();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.btnEjec = new System.Windows.Forms.Button();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblNumero1
+            // 
+            this.lblNumero1.AutoSize = true;
+            this.lblNumero1.Location = new System.Drawing.Point(142, 52);
+            this.lblNumero1.Name = "lblNumero1";
+            this.lblNumero1.Size = new System.Drawing.Size(56, 13);
+            this.lblNumero1.TabIndex = 0;
+            this.lblNumero1.Text = "Numero 1:";
+            // 
+            // lblNumero2
+            // 
+            this.lblNumero2.AutoSize = true;
+            this.lblNumero2.Location = new System.Drawing.Point(142, 94);
+            this.lblNumero2.Name = "lblNumero2";
+            this.lblNumero2.Size = new System.Drawing.Size(56, 13);
+            this.lblNumero2.TabIndex = 1;
+            this.lblNumero2.Text = "Numero 2:";
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Location = new System.Drawing.Point(142, 149);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(0, 13);
+            this.lblRes.TabIndex = 2;
+            // 
+            // btnEjec
+            // 
+            this.btnEjec.Location = new System.Drawing.Point(337, 66);
+            this.btnEjec.Name = "btnEjec";
+            this.btnEjec.Size = new System.Drawing.Size(75, 23);
+            this.btnEjec.TabIndex = 3;
+            this.btnEjec.Text = "Comprobar";
+            this.btnEjec.UseVisualStyleBackColor = true;
+            this.btnEjec.Click += new System.EventHandler(this.btnEjec_Click);
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(215, 45);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero1.TabIndex = 4;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(215, 87);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 226);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.btnEjec);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.lblNumero2);
+            this.Controls.Add(this.lblNumero1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumero1;
+        private System.Windows.Forms.Label lblNumero2;
+        private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.Button btnEjec;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+    }
+}
+
---
+++ NúmeroMayorOIgual/NúmeroMayorOIgual/Vistas/Form1.cs
@@ -0,0 +1,34 @@
+﻿using NúmeroMayorOIgual.Entidades;
+using NúmeroMayorOIgual.Negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NúmeroMayorOIgual
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEjec_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(txtNumero1.Text);
+            int n2 = Convert.ToInt32(txtNumero2.Text);
+
+            Numeros num = new Numeros(n1, n2);
+
+            Logica log = new Logica();
+            String r = log.NMayorOIgual(num);
+            lblRes.Text = r;
+        }
+    }
+}
